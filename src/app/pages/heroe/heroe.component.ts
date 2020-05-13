75de5c6dc9bf3b203382a18eb333dc5e6e12ea00
Cristian Angulo <paul.angulo.anyosa@gmail.com>
2020-05-13 04:22:44 -0500

    219. HTTP - POST - Creando un nuevo registro en Firebase

---
+++ src/app/pages/heroe/heroe.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroeModel } from 'src/app/models/heroe.model';
+import { NgForm } from '@angular/forms';
+import { HeroesService } from 'src/app/services/heroes.service';
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+  styleUrls: ['./heroe.component.css']
+})
+export class HeroeComponent implements OnInit {
+
+  heroe: HeroeModel = new HeroeModel();
+
+  constructor( private heroeService: HeroesService ) { }
+
+  ngOnInit(): void {
+  }
+
+  guardar(form: NgForm) {
+
+    if ( form.invalid ) {
+      console.log( 'Formulario no valido' );
+      return;
+    }
+
+    this.heroeService.crearHeroe( this.heroe ).subscribe( ( resp ) => {
+      console.log( resp );
+    } );
+
+  }
+
+}
